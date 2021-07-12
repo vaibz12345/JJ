b72861264e7d98c86f44dcec8757a16e00985ed3
vaibz12345 <vaibhavsingh161997@gmail.com>
2021-07-12 22:43:18 +0530

    JJ Commit

---
+++ JJ/src/main/java/com/appsdeveloperblog/photoapp/api/JjApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjApplication.class, args);
+	}
+
+}
